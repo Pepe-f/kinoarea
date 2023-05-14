97e0656071193c6e51904486a25e2ee1e0f4fde5
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2023-05-14 19:45:19 +0300

    Storybook, Global styles, Button component

---
+++ src/components/UI/ButtonBase/ButtonBase.tsx
@@ -0,0 +1,30 @@
+import { ButtonHTMLAttributes, ReactNode, memo } from 'react';
+import cn from 'classnames';
+import styles from './ButtonBase.module.scss';
+
+interface ButtonBaseProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  animationDuration?: number;
+}
+
+export const ButtonBase = memo<ButtonBaseProps>(
+  ({
+    className,
+    startIcon = null,
+    endIcon = null,
+    animationDuration = 500,
+    children,
+    ...props
+  }) => {
+    return (
+      <button className={cn(styles.btn, className)} {...props}>
+        {startIcon && <span className={styles.startIcon}>{startIcon}</span>}
+        {children}
+        {endIcon && <span className={styles.endIcon}>{endIcon}</span>}
+      </button>
+    );
+  }
+);
+
+ButtonBase.displayName = 'ButtonBase';
